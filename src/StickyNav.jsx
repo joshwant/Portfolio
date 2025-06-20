4f583c16277d5288a912e8f8688fa2707bd29bcb
joshwant <joshua.want@students.plymouth.ac.uk>
2025-06-20 23:54:06 +0100

    Improved Sticky Nav Bar size

--- src/StickyNav.jsx
+++ src/StickyNav.jsx
@@ -6,12 +6,12 @@ export default function StickyNav({ onNavClick, visible, activeSection }) {
       className={`fixed top-4 left-1/2 transform -translate-x-1/2 transition-opacity duration-500 z-50
         ${visible ? "opacity-100" : "opacity-0 pointer-events-none"}`}
     >
-      <div className="bg-white/80 backdrop-blur-md shadow-lg rounded-full px-6 py-3 flex space-x-6">
+      <div className="bg-white/80 backdrop-blur-md shadow-lg rounded-full px-4 py-2 flex space-x-3">
         {["home", "projects", "work", "contact"].map((section) => (
           <button
             key={section}
             onClick={() => onNavClick(section)}
-            className={`px-5 py-2 rounded-full text-xl transition font-bold 
+            className={`px-3 py-1 rounded-full text-m transition font-semibold 
               ${
                 section === activeSection
                   ? "border-2 border-gray-900 text-gray-900"
