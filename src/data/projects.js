b97b01c608ff55c47ab4b170258a74e51cbd29e2
joshwant <joshua.want@students.plymouth.ac.uk>
2026-01-23 02:41:20 +0000

    Improving Projects Modularity

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,16 @@
-import posterImage from "../assets/prepit/poster.jpg";
-import homepageImage from "../assets/prepit/homepage.png";
+import prepitThumb from "../assets/prepit/poster.jpg";
+import prepit1 from "../assets/prepit/homepage.png";
 
-import toothtrackHomepage from "../assets/toothtrack/homepage.jpg";
+import toothThumb from "../assets/toothtrack/homepage.jpg";
+import tooth1 from "../assets/toothtrack/tooth1.png";
 
 export const projectsData = [
   {
     id: "prepit",
     title: "PrepIt",
     shortDescription: "AI-Powered Meal Planning and Price Comparison.",
-    mainImage: posterImage,
-    images: [posterImage, homepageImage],
+    mainImage: prepitThumb,
+    images: [prepitThumb, prepit1],
     description: "PrepIt is a mobile app that allows users to explore recipes...",
     github: "https://github.com/joshwant/...",
     technologies: ["React Native", "Expo", "Node.js", "MongoDB"],
@@ -19,8 +20,8 @@ export const projectsData = [
     id: "toothtrack",
     title: "ToothTrack",
     shortDescription: "Dental Care Mobile App to Simplify Patient Experience.",
-    mainImage: posterImage,
-    images: [toothtrackHomepage, homepageImage],
+    mainImage: toothThumb,
+    images: [toothThumb, tooth1],
     description: "ToothTrack is a mobile app that offers convenient appointment scheduling, secure communication, easy access to dental records, order dental supplies, and more...",
     github: "https://github.com/joshwant/...",
     technologies: ["Flutter", "Dart", "Node.js"],
